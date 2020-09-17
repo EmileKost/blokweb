f008131f3d7c4c495610c2511316e76a06c5bd2c
EmileKost <70690100+EmileKost@users.noreply.github.com>
2020-09-17 09:55:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+
+var button = document.querySelector('button');
+var element = document.querySelector('body');
+
+button.addEventListener("click", function() {
+  element.classlist.add('anders');
+});
